b1aa31cef37de4cada21a2f84c54160549160844
Bohdan Klochko <bohdanklochko1@gmail.com>
2024-02-19 14:21:14 +0200

    add roundBy method

--- src/Money.php
+++ src/Money.php
@@ -145,6 +145,16 @@ class Money implements Arrayable, Jsonable, JsonSerializable, Renderable
         );
     }
 
+    public function roundBy(int $number, bool $up): self
+    {
+        $result = match ($up) {
+            false => fn ($price) => $number * round($this->getAmount() / $number),
+            true => fn ($price) => $number * ceil($this->getAmount() / $number),
+        };
+
+        return new self((int) $result, $this->getCurrency());
+    }
+
     public static function min(mixed $first, mixed ...$collection): self
     {
         $first = self::parse($first)->money;
